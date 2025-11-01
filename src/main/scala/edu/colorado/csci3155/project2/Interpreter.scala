910ee94233cce95fece550a55066cfe2deca3eae
Mitch Avery <averymr@colorado.edu>
2025-11-01 00:06:01 -0600

    finished project 2

--- src/main/scala/edu/colorado/csci3155/project2/Interpreter.scala
+++ src/main/scala/edu/colorado/csci3155/project2/Interpreter.scala
@@ -12,14 +12,82 @@ object Interpreter {
         case Const(d) => NumValue(d)
         case ConstBool(b) => BoolValue(b)
         case Ident(s) => env.lookup(s)
-        case Line(l) => ??? //TODO: Handle a line object
-        case EquiTriangle(sideLength) => ??? // TODO: Handle Equilateral Triangle
-        case Rectangle(sideLength) => ??? // TODO: Handle square given the side length
-        case Circle(rad) => ??? //TODO: Handle circle
-        case Plus (e1, e2) => ??? // TODO: Handle addition of numbers or figures
+
+        // case Line(l) => {
+        //     evalExpr(l, env) match ----???
+        case Line(l) => {
+
+            val v = evalExpr(l, env)
+            v match {
+                case NumValue(v) => 
+                    val line_obj = Polygon(List((0.0, 0.0), (v, 0.0)))
+                    FigValue(new MyCanvas(List(line_obj)))
+                case _ => throw new IllegalArgumentException("Requires a numeric value")
+            }
+        }
+        case EquiTriangle(sideLength) => {
+            val v = evalExpr(sideLength, env)
+            v match {
+                case NumValue(v) =>
+                    val equiTri_obj = Polygon(List((0.0, 0.0), (v, 0.0), (v/2, math.sqrt(3.0)*v / 2)))
+                    FigValue(new MyCanvas(List(equiTri_obj)))
+                case _ => throw new IllegalArgumentException("Requires a numeric value")
+            }
+
+            
+        }
+        case Rectangle(sideLength) => {
+            val v = evalExpr(sideLength, env)
+            v match {
+                case NumValue(v) =>
+                    val rect_obj = Polygon(List((0.0, 0.0), (0.0, v), (v, v), (v, 0.0)))
+                    FigValue(new MyCanvas(List(rect_obj)))
+                case _ => throw new IllegalArgumentException("Requires a numeric value")
+            }
+        }
+
+        case Circle(rad) => {
+            val v = evalExpr(rad, env)
+            v match {
+                case NumValue(v) =>
+                    val circle_obj = MyCircle((v, v), v)
+                    FigValue(new MyCanvas(List(circle_obj)))
+                case _ => throw new IllegalArgumentException("Requires a numeric value")
+            }    
+        }
+
+        case Plus (e1, e2) => {
+            val v1 = evalExpr(e1, env)
+            val v2 = evalExpr(e2, env)
+            (v1, v2) match {
+                case (NumValue(v1), NumValue(v2)) => NumValue(v1 + v2)
+                case (FigValue(c1), FigValue(c2)) => FigValue(c1.overlap(c2))
+                case _ => throw new IllegalArgumentException("Addition requires two NumValues or two FigValues")
+            }
+        }
         case Minus (e1, e2) => binaryExprEval(e1, e2, env) (ValueOps.minus)
-        case Mult(e1, e2) => ??? // TODO: Handle multiplication of numbers or figures
-        case Div(e1, e2) => ??? // TODO: Handle division
+        case Mult(e1, e2) => {
+            val v1 = evalExpr(e1, env)
+            val v2 = evalExpr(e2, env)
+            (v1, v2) match {
+                case (NumValue(v1), NumValue(v2)) => NumValue(v1 * v2)
+                case (FigValue(c1), FigValue(c2)) => FigValue(c1.placeRight(c2))
+                case _ => throw new IllegalArgumentException("Multiplication requires either two NumValues or two FigValues")
+            }
+        }
+
+        case Div(e1, e2) => {
+            val v1 = evalExpr(e1, env)
+            val v2 = evalExpr(e2, env)
+            (v1, v2) match {
+                case (NumValue(v1), NumValue(v2)) => 
+                    if (v2 == 0.0) throw new IllegalArgumentException("Cannot divide by zero")
+                    else NumValue(v1 / v2)
+                case (FigValue(c), NumValue(v)) => FigValue(c.rotate(v))
+                case (FigValue(c1), FigValue(c2)) => FigValue(c2.placeTop(c1))
+                case _ => throw new IllegalArgumentException("Division requires either two NumValues or two FigValues")
+            }    
+        }
         case Geq(e1, e2) => binaryExprEval(e1, e2, env) (ValueOps.geq)
         case Gt(e1, e2) => binaryExprEval(e1, e2, env) (ValueOps.gt)
         case Eq(e1, e2) => binaryExprEval(e1, e2, env) (ValueOps.equal)
@@ -78,9 +146,19 @@ object Interpreter {
             evalExpr(e2, env2)
         }
 
-        case FunDef(x, e) => ??? //TODO: Handle function definitions
-        case LetRec(f, x, e1, e2) => ??? // TODO: Handle recursive functions -- look at Environment.scala
-        case FunCall(fCallExpr, arg) => ??? // TODO: Handle function calls
+        case FunDef(x, e) => Closure(x, e, env)
+        case LetRec(f, x, e1, e2) => {
+            val env1 = ExtendREC(f, x, e1, env)
+            evalExpr(e2, env1)
+        }
+        case FunCall(fCallExpr, arg) => {
+            val f1 = evalExpr(fCallExpr, env)
+            f1 match {
+                case Closure(x, e, env1) =>
+                    val argEval = evalExpr(arg, env)
+                    evalExpr(e, Extend(x, argEval, env1))
+            }   
+        }
     }
 
     def evalProgram(p: Program): Value = p match {
